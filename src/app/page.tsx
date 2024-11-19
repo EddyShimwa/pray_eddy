0fc6ec6fa297d5c4b240017acc13d578765b0ca6
EddyShimwa <shedpray16@gmail.com>
2024-11-19 20:01:19 +0200

    USE the header with dark theme toggler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,11 @@ import Image from "next/image";
 import HomePage from "./sections/page";
 import AboutPage from "./sections/about";
 import Projects from "./sections/projects";
-import NavBar from "@/components/ui/navbar";
-
+import Header from "./Header";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <NavBar />
+    <main className="flex min-h-screen w-full flex-col items-center justify-between py-8 px-24">
+     <Header logo="Eddy" />
       <HomePage />
       <Projects />
       <AboutPage />
--- src/app/sections/page.tsx
+++ src/app/sections/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>
-      <p>Hello from main page</p>
+<div>
+  <h1>Hey</h1>
     </div>
   )
 }
